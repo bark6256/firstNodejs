8deeb01e929bf4d7a3321d577302331aee57794c
pys <bark6256@naver.com>
2022-10-31 15:11:31 +0900

    백엔드의 데이터를 웹페이지에 전송

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,6 @@
 "use strict";
+
+
 const output = {
     home : (req, res) => {
         res.render("home/index");
@@ -9,9 +11,29 @@ const output = {
     }
 };
 
+const users = {
+    id: ["1010", "2020", "3030"],
+    password: ["1q2w3e", "1q2w3e","1q2w3e4r"]
+};
+
 const process = {
     login : (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+          password = req.body.password;
+        
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다.",
+        });
     }
 };
 
